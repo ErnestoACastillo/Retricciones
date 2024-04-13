d02e25bb5995661bdd4396893c922621cb725184
ErnestoACastillo <148025891+ErnestoACastillo@users.noreply.github.com>
2024-04-13 05:01:20 -0400

    Cambios de codigo Test.py

--- Test.py
+++ Test.py
@@ -3,7 +3,7 @@ from app import suma
 
 class TestSuma(unittest.TestCase):
     def test_suma(self):
-        self.assertEqual(suma(1, 2), 3)
+        self.assertEqual(suma(1, 2, 3, 4)
 
 if __name__ == '__main__':
     unittest.main()
